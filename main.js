381b05b3ead06182c804829df8f3819dd6f07c72
Fernandinha14 <106314872+Fernandinha14@users.noreply.github.com>
2024-03-11 12:23:46 -0300

    nanda

---
+++ main.js
@@ -0,0 +1,13 @@
+const botoes= document.queryselectorAll (".botao");
+
+for (let i=0; i <botoes.length; i++) {
+
+    botoes [i]. onclik=function ()  {
+
+        for (let j=0; j <botoes.length; j++) {
+            botoes[j] .classlist.remove("ativo");
+    }
+    botoes[i] .classlist.add("ativo");
+
+    }
+}
